888a26a785d3edc8ba8557df54fd38078ced53e4
Omer Akram <om26er@ubuntu.com>
2014-07-08 00:06:17 +0500

    small stuff

--- tests/autopilot/messaging_app/fixture_setup.py
+++ tests/autopilot/messaging_app/fixture_setup.py
@@ -1,12 +1,11 @@
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
-# Copyright 2013, 2014 Canonical
+# Copyright 2014 Canonical
 #
 # This file is part of messaging-app.
 #
 # messaging-app is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 3, as published
 # by the Free Software Foundation.
-
 
 import fixtures
 import subprocess
