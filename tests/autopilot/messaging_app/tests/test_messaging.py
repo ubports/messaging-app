96bd260df673795c3f0b73886b3442a38e986dae
Iftikhar Ahmad <iftikhar.ahmad@canonical.com>
2014-07-14 12:49:55 +0000

    Added test for newly added helper get_contact_list_view

--- tests/autopilot/messaging_app/tests/test_messaging.py
+++ tests/autopilot/messaging_app/tests/test_messaging.py
@@ -90,6 +90,17 @@ class BaseMessagingTestCase(MessagingAppTestCase):
 class TestMessaging(BaseMessagingTestCase):
     """Tests for the communication panel."""
 
+    def test_helper_get_contact_list_view(self):
+        """test get_contact_list_view() helper is working"""
+        # open the chat window
+        self.main_view.start_new_message()
+        add_icon = self.main_view.get_toolbar_add_contact_icon()
+        self.pointing_device.click_object(add_icon)
+
+        # pop up the contact list to choose recipient
+        contact_view = self.main_view.get_contact_list_view()
+        self.assertThat(contact_view.visible, Eventually(Equals(True)))
+
     def test_write_new_message_to_group(self):
         recipient_list = ["123", "321"]
         self.main_view.start_new_message()
