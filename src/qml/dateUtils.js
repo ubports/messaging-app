18f0c9149c7c4cfcc95cd63600952bdb5f4fef58
Renato Araujo Oliveira Filho <renato.filho@canonical.com>
2014-08-26 15:51:33 -0300

    Avoid compare 'undefined' dates.

--- src/qml/dateUtils.js
+++ src/qml/dateUtils.js
@@ -17,6 +17,9 @@
  */
 
 function areSameDay(date1, date2) {
+    if (!date1 || !date2)
+        return false
+
     return date1.getFullYear() == date2.getFullYear()
         && date1.getMonth() == date2.getMonth()
         && date1.getDate() == date2.getDate()
