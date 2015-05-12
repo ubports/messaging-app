8c213ca5f10fb7d041280ebf949638c828fb131a
Renato Araujo Oliveira Filho <renato.filho@canonical.com>
2015-05-12 17:22:31 -0300

    Fixed autopilot.

--- tests/autopilot/messaging_app/fixture_setup.py
+++ tests/autopilot/messaging_app/fixture_setup.py
@@ -188,7 +188,7 @@ class UseMemoryContactBackend(fixtures.Fixture):
 
 
 class PreloadVcards(fixtures.Fixture):
-    VCARD_PATH_BIN = "/usr/share/messagin-app/vcards/vcard.vcf"
+    VCARD_PATH_BIN = "/usr/share/messaging-app/vcards/vcard.vcf"
     VCARD_PATH_DEV = os.path.abspath("../data/vcard.vcf")
 
     def setUp(self):
